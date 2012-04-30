4a5a1951fa243892f235a89ad707bc20598ad37f
max <maxe.ludwig@googlemail.com>
2012-04-30 21:35:04 +0200

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ webrc
 =====
 
 A web interface to remote control mpris players and pulseaudio volume
+
+## Usage
+
+Create a python virtualenv, install Flask in it and run `python music.py`
---
+++ music.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect
+import os
+import dbus
+import re
+import subprocess
+
+app = Flask(__name__)
+
+org_mpris2_re = re.compile('^org\.mpris\.MediaPlayer2\.([^.]+)$')
+bus = dbus.SessionBus()
+
+@app.route("/", methods=["GET"])
+def index():
+    players = [ name for name in bus.list_names() if org_mpris2_re.match(name)]
+    info = subprocess.check_output(["python", "/home/max/bin/mpris-remote"])
+    return render_template('index.html', players=players, info=info)
+
+@app.route("/volume/<x>")
+def volume(x):
+    try:
+        int(x)
+        os.system("pactl -- set-sink-volume 0 %s%%" % x)
+    except:
+        pass
+    return redirect("/")
+
+@app.route("/playback/<x>")
+def playback(x):
+    if x in ["play", "pause", "prev", "next", "stop", "playpause"]:
+        os.system("python /home/max/bin/mpris-remote %s" % x)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=31337)
